4179dcf375712b4aca52db3edb76bdcbda29c546
Valentin <valentinfvazquez@gmail.com>
2023-01-13 14:54:45 -0300

    santi

--- pages/jsonview.tsx
+++ pages/jsonview.tsx
@@ -1,28 +1,26 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from 'next/head';
+import React from 'react';
+import { JsonViewer } from '@textea/json-viewer';
 import { getProcessedDataSet } from '../lib/getProcessedDataSet';
-import { JsonViewer } from '@textea/json-viewer'
-
 
 export default function JsonView(props: any) {
-
   return (
     <>
       <Head>
         <title>WGS - Leniolabs</title>
       </Head>
       <main>
-        <JsonViewer defaultInspectDepth={1} value={props.processedData}/>
+        <JsonViewer defaultInspectDepth={1} value={props.processedData} />
       </main>
     </>
-  )
+  );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const processedData = await getProcessedDataSet();
   return {
     props: {
       processedData,
-    }
-  }
+    },
+  };
 }
