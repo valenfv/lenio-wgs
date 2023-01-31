a427b5c26d0ddedfc523874df2952d903e29025f
Valentin <valentinfvazquez@gmail.com>
2023-01-31 13:47:36 -0300

    improvements

--- components/RadialChart.jsx
+++ components/RadialChart.jsx
@@ -667,7 +667,7 @@ function RadialChart() {
         .attr('height', '68')
         .attr('viewBox', '0 0 47 68')
         .attr('d', 'M0.85908 23.9301C0.808557 23.8311 0.77669 23.7396 0.76432 23.6292C0.5416 21.6415 -1.20447 0.746582 23.8168 0.746582C48.8381 0.746582 47.092 21.6415 46.8693 23.6292C46.8569 23.7396 46.8251 23.8311 46.7745 23.9301L24.7074 67.1449C24.3362 67.8718 23.2974 67.8718 22.9262 67.1449L0.85908 23.9301Z')
-        .attr('fill', (d) => {
+        .attr('fill', () => {
           const pinColor = INDICATORS_TYPE_MAP[LABELS_MAP[indicators[selectedIndicator].indicator_name].type];
           return pinColor;
         })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { ConfigContainer } from '../components/ConfigContainer';
 import RadialChart from '../components/RadialChart';
 import styles from '../styles/commons.module.css';
+import { Loading } from '../components/Loading';
 
 export default function Home() {
+  const loading = useSelector((state) => state.radialChart.loading);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ position: 'relative' }}>
       <div className={styles.menu}>
         <ConfigContainer showAxisSelection={false} makeIndicatorListClickable />
       </div>
+      <Loading loading={loading} />
       <RadialChart />
     </div>
   );
