9a1d527c80903e9d1a7fa06aeca5e5f7078170b1
gustavo rojas <garv1998131@gmail.com>
2023-01-31 09:02:59 -0300

    fixed lint issues

--- utils/rankingUtils.ts
+++ utils/rankingUtils.ts
@@ -1,57 +1,55 @@
-import countries_data from "../data/data.json";
-import { CountriesData, CountriesDataKeys, CountryRankingData, IndicatorsKeys } from "../interfaces";
+import countriesDataJson from '../data/data.json';
+import {
+  CountriesData, CountriesDataKeys, CountryRankingData, IndicatorsKeys,
+} from '../interfaces';
 
 function sortYears(years: Array<string>) {
-	years.sort((a, b) => Number(b) - Number(a));
-	return years;
+  years.sort((a, b) => Number(b) - Number(a));
+  return years;
 }
 
 export function round2Decimals(num: number) {
-	return Math.round((num + Number.EPSILON) * 100) / 100;
+  return Math.round((num + Number.EPSILON) * 100) / 100;
 }
 
-export function getRankingPosition(arr: any, comparing_country: CountriesDataKeys) {
-	const idx = arr.findIndex((metric: CountryRankingData) => metric.country === comparing_country);
-	return idx + 1;
+export function getRankingPosition(arr: any, comparingCountry: CountriesDataKeys) {
+  const idx = arr.findIndex((metric: CountryRankingData) => metric.country === comparingCountry);
+  return idx + 1;
 }
 
 export function rankingSort(arr: any, isHigherBetter: boolean) {
-	const arrCopy = [...arr];
-	if (isHigherBetter) {
-		arrCopy.sort((a: CountryRankingData, b: CountryRankingData) => {
-			if (!a || !a.value) {
-				return 1;
-			}
-			if (!b || !b.value) {
-				return -1;
-			}
-			return b?.value - a?.value;
-		});
-	} else {
-		arrCopy.sort((a: CountryRankingData, b: CountryRankingData) => {
-			if (!a || !a.value) {
-				return 1;
-			}
-			if (!b || !b.value) {
-				return -1;
-			}
-			return a?.value - b?.value;
-		});
-	}
-	return [...arrCopy];
+  const arrCopy = [...arr];
+  if (isHigherBetter) {
+    arrCopy.sort((a: CountryRankingData, b: CountryRankingData) => {
+      if (!a || !a.value) return 1;
+      if (!b || !b.value) return -1;
+      return b.value - a.value;
+    });
+  } else {
+    arrCopy.sort((a: CountryRankingData, b: CountryRankingData) => {
+      if (!a || !a.value) return 1;
+      if (!b || !b.value) return -1;
+      return a.value - b.value;
+    });
+  }
+  return arrCopy;
 }
 
-export function getIndicatorValue(country: CountriesDataKeys, indicator: IndicatorsKeys, year_pos = 0) {
-	const country_indicators = (countries_data as CountriesData)[country]?.indicators?.[indicator];
-	if (!country_indicators) return null;
-	const selected_year = sortYears(Object.keys(country_indicators))[year_pos];
-	if (selected_year) {
-		const value = (countries_data as CountriesData)[country].indicators[indicator][selected_year];
-		return {
-			value,
-			year: selected_year,
-		};
-	}
+export function getIndicatorValue(
+  country: CountriesDataKeys,
+  indicator: IndicatorsKeys,
+  yearPos = 0,
+) {
+  const countryIndicators = (countriesDataJson as CountriesData)[country]?.indicators?.[indicator];
+  if (!countryIndicators) return null;
+  const selectedYear = sortYears(Object.keys(countryIndicators))[yearPos];
+  if (selectedYear) {
+    const value = (countriesDataJson as CountriesData)[country].indicators[indicator][selectedYear];
+    return {
+      value,
+      year: selectedYear,
+    };
+  }
 
-	return null;
+  return null;
 }
